0bd3ab49e6f7d6bfe90d3d0a1621f6cba43bb58e
aTaruna <aputrataruna@gmail.com>
2022-04-07 07:45:04 +0700

    PAM

---
+++ app/src/main/java/com/example/act6/database/TemanBaru.java
@@ -0,0 +1,55 @@
+package com.example.act6.database;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.act6.MainActivity;
+import com.example.act6.R;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.HashMap;
+
+public class TemanBaru extends AppCompatActivity {
+    private TextInputEditText tNama,tTelepon;
+    private Button simpanBtn;
+    String nm,tlp;
+    DBController controller=new DBController(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teman_baru);
+
+        tNama=(TextInputEditText) findViewById(R.id.tietNama);
+        tTelepon=(TextInputEditText) findViewById(R.id.tietTelepon);
+        simpanBtn=(Button) findViewById(R.id.buttonSave);
+
+        simpanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (tNama.getText().toString().equals("")||tTelepon.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(),"Data kurang",Toast.LENGTH_SHORT).show();
+                }else {
+                    nm=tNama.getText().toString();
+                    tlp=tTelepon.getText().toString();
+
+                    HashMap<String,String> qvalues=new HashMap<>();
+                    qvalues.put("nama",nm);
+                    qvalues.put("telepon",tlp);
+                    controller.insertData(qvalues);
+                    callHome();
+                }
+            }
+        });
+    }
+    public void callHome(){
+        Intent intent=new Intent(TemanBaru.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
